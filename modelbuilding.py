cf3ae3ae8dd14bcc12021e54344f08f88385970d
lawalsegun2025 <lawalsegun2025@gmail.com>
2023-07-05 10:51:36 +0100

    all

--- modelbuilding.py
+++ modelbuilding.py
@@ -19,3 +19,18 @@ def split_data(df):
     X_train, X_test, y_train, y_test = train_test_split(np.array(X), y, test_size=0.2)
 
     return X_train, X_test, y_train, y_test
+
+# Logistic Regression
+
+def logistic(df):
+
+    X_train, X_test, y_train, y_test = split_data(df)
+    lr = LogisticRegression()
+    lr.fit(X_train, y_train)
+    y_pred = lr.predict(X_test)
+    accuracy = metrics.accuracy_score(y_test, y_pred)
+    recall = metrics.recall_score(y_test, y_pred, average="macro")
+    precision = metrics.precision_score(y_test, y_pred, average="macro")
+    f1score = metrics.f1_score(y_test, y_pred, average="macro")
+    
+    return accuracy, recall, precision, f1score, lr
