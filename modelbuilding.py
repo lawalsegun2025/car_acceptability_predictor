af1643c13ea5d19b31fc807542d3117e817c1f15
lawalsegun2025 <lawalsegun2025@gmail.com>
2023-07-05 09:15:35 +0100

    all

---
+++ modelbuilding.py
@@ -0,0 +1,8 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn import metrics
+import pickle
